e26765e0ad1c20ac30fb9738cfc7131998dd6186
Aaron Thomas <hampeaaron@gmail.com>
2026-06-06 17:05:54 +0200

    working on projects and fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -182,7 +182,7 @@ export default function Footer() {
               <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-400 opacity-75"></span>
               <span className="relative inline-flex rounded-full h-2 w-2 bg-emerald-500"></span>
             </span>
-            <span>All systems operational</span>
+            <span>Alle Systeme betriebsbereit</span>
           </div>
         </div>
       </div>
